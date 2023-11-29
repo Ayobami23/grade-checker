f89be073aeaa71556b57d139e5fe0f0e3007ee45
Emmanuel <oluwadareayobamiemmanuel@gmail.com>
2023-11-29 15:04:18 +0000

    This is the  Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const input = document.getElementById('input');
+const result = document.getElementById('result');
+const form = document.getElementById('form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  input.value >= 70
+    ? (result.textContent = 'A')
+    : input.value >= 60
+    ? (result.textContent = 'B')
+    : input.value >= 50
+    ? (result.textContent = 'C')
+    : input.value >= 45
+    ? (result.textContent = 'D')
+    : input.value >= 40
+    ? (result.textContent = 'E')
+    : (result.textContent = 'F');
+});
